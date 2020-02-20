41875b08822caa7a23122b6b860c274fec02a6da
Hilde Steinbru Heggstad <hilde.steinbru.heggstad@nav.no>
2020-02-20 11:58:30 +0100

    kjort testcontroller

---
+++ innsynAaregApi/src/main/kotlin/no/nav/tag/innsynAareg/controller/Test.kt
@@ -0,0 +1,17 @@
+package no.nav.tag.innsynAareg.controller
+
+import org.springframework.http.ResponseEntity
+import org.springframework.web.bind.annotation.*
+
+@RestController
+class Test {
+
+    @GetMapping(value = ["/test"])
+    @ResponseBody
+    fun skrivTest(): ResponseEntity<String> {
+
+        return ResponseEntity.ok<String>("hei")
+    }
+
+}
+
