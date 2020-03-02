b4cb55e3fc1731580bfba51608a01484ed70f77d
Hilde Steinbru Heggstad <hilde.steinbru.heggstad@nav.no>
2020-03-02 15:10:30 +0100

    lagt til nytt navn og kontekstpath

--- src/main/kotlin/no/nav/tag/innsynAareg/InnsynAaregApplication.kt
+++ src/main/kotlin/no/nav/tag/innsynAareg/InnsynAaregApplication.kt
@@ -4,8 +4,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
 
 @SpringBootApplication
-class DemoApplication
+class InnsynAaregApplication
 
 fun main(args: Array<String>) {
-    runApplication<DemoApplication>(*args)
+    runApplication<InnsynAaregApplication>(*args)
 }
