3afd2e31bcc97ce7ff92ec5b8cf7db32e1983238
Hilde Steinbru Heggstad <hilde.steinbru.heggstad@nav.no>
2020-03-06 11:53:13 +0100

    lagt til STS service

--- src/main/kotlin/no/nav/tag/innsynAareg/services/sts/STSClient.kt
+++ src/main/kotlin/no/nav/tag/innsynAareg/services/sts/STSClient.kt
@@ -1,71 +1,67 @@
-package no.nav.tag.innsynAareg.services.sts;
+package no.nav.tag.innsynAareg.services.sts
 
-import lombok.extern.slf4j.Slf4j;
-import no.nav.tag.innsynAareg.InnsynAaregApplication;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Value;
-import org.springframework.cache.annotation.CacheEvict;
-import org.springframework.cache.annotation.Cacheable;
-import org.springframework.http.*;
-import org.springframework.stereotype.Component;
-import org.springframework.web.client.HttpClientErrorException;
+import lombok.extern.slf4j.Slf4j
+import no.nav.tag.innsynAareg.InnsynAaregApplication
+import no.nav.tag.innsynAareg.services.sts.STSCacheConfig.Companion.STS_CACHE
 
-import org.springframework.web.client.RestTemplate;
-import org.springframework.web.util.UriComponentsBuilder;
-
-import static no.nav.tag.innsynAareg.services.sts.StsCacheConfig.STS_CACHE;
-
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.beans.factory.annotation.Value
+import org.springframework.cache.annotation.CacheEvict
+import org.springframework.cache.annotation.Cacheable
+import org.springframework.http.*
+import org.springframework.stereotype.Component
+import org.springframework.web.client.HttpClientErrorException
+import org.springframework.web.client.RestTemplate
+import org.springframework.web.util.UriComponentsBuilder
 
 @Slf4j
 @Component
-class STSClient {
+class STSClient @Autowired
+constructor(@Value("\${sts.stsPass}") stsPass: String,
+            @Value("\${sts.stsUrl}") stsUrl: String,
+            private val restTemplate: RestTemplate) {
+    private val requestEntity: HttpEntity<String>
+    private val uriString: String
 
-    val RestTemplate restTemplate;
-    val HttpEntity<String> requestEntity;
-    val String uriString;
-
-    @Autowired
-    public STSClient(@Value("${sts.stsPass}") String stsPass,
-    @Value("${sts.stsUrl}") String stsUrl,
-    RestTemplate restTemplate) {
-        this.restTemplate = restTemplate;
-        this.requestEntity = getRequestEntity(stsPass);
-        this.uriString = buildUriString(stsUrl);
-    }
-
-    private HttpEntity<String> getRequestEntity(String stsPass) {
-        HttpHeaders headers = new HttpHeaders();
-        headers.setBasicAuth(InnsynAaregApplication.APP_NAME, stsPass);
-        headers.setContentType(MediaType.APPLICATION_FORM_URLENCODED);
-        return new HttpEntity<>(headers);
-    }
-
-    private String buildUriString(String stsUrl) {
-        return UriComponentsBuilder.fromHttpUrl(stsUrl)
-                .queryParam("grant_type","client_credentials")
-                .queryParam("scope","openid")
-                .toUriString();
-    }
-
-    @Cacheable(STS_CACHE)
-    public STStoken getToken() {
-        try {
-            ResponseEntity<STStoken> response = restTemplate.exchange(uriString, HttpMethod.GET, requestEntity, STStoken.class);
-            if(response.getStatusCode() != HttpStatus.OK){
-                String message = "Kall mot STS feiler med HTTP-" + response.getStatusCode();
-                log.error(message);
-                throw new RuntimeException(message);
+    val token: STStoken?
+        @Cacheable(STS_CACHE)
+        get() {
+            try {
+                val response = restTemplate.exchange(uriString, HttpMethod.GET, requestEntity, STStoken::class.java)
+                if (response.getStatusCode() != HttpStatus.OK) {
+                    val message = "Kall mot STS feiler med HTTP-" + response.getStatusCode()
+                    //log.error(message)
+                    throw RuntimeException(message)
+                }
+                return response.getBody()
+            } catch (e: HttpClientErrorException) {
+                //log.error("Feil ved oppslag i STS", e)
+                throw RuntimeException(e)
             }
-            return (response.getBody());
+
         }
-        catch(HttpClientErrorException e){
-            log.error("Feil ved oppslag i STS", e);
-            throw new RuntimeException(e);
-        }
+
+    init {
+        this.requestEntity = getRequestEntity(stsPass)
+        this.uriString = buildUriString(stsUrl)
+    }
+
+    private fun getRequestEntity(stsPass: String): HttpEntity<String> {
+        val headers = HttpHeaders()
+        headers.setBasicAuth(InnsynAaregApplication.APP_NAME, stsPass)
+        headers.contentType = MediaType.APPLICATION_FORM_URLENCODED
+        return HttpEntity(headers)
+    }
+
+    private fun buildUriString(stsUrl: String): String {
+        return UriComponentsBuilder.fromHttpUrl(stsUrl)
+                .queryParam("grant_type", "client_credentials")
+                .queryParam("scope", "openid")
+                .toUriString()
     }
 
     @CacheEvict(STS_CACHE)
-    public void evict() {
+    fun evict() {
     }
 
 }
