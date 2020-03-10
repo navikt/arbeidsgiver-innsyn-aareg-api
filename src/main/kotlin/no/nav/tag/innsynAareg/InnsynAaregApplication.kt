69e830c145d618acaa0056c6b4c156320c7cde4a
Ibenholt <bendik.segrov.ibenholt@nav.no>
2020-03-10 13:08:51 +0100

    legg til oidc verifisering

--- src/main/kotlin/no/nav/tag/innsynAareg/InnsynAaregApplication.kt
+++ src/main/kotlin/no/nav/tag/innsynAareg/InnsynAaregApplication.kt
@@ -1,9 +1,11 @@
 package no.nav.tag.innsynAareg
 
+import no.nav.security.spring.oidc.api.EnableOIDCTokenValidation
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
 
 @SpringBootApplication
+@EnableOIDCTokenValidation(ignore=["org.springframework"])
 class InnsynAaregApplication
 
 fun main(args: Array<String>) {
