281c79afd569b08bee71c9476bf7fd9d8fbbe60b
Ibenholt <bendik.segrov.ibenholt@nav.no>
2020-04-07 12:23:23 +0200

    endret yrkeskoderespons tilbake slik det var opprinnelig

--- src/main/kotlin/no/nav/tag/innsynAareg/models/Yrkeskoderespons/Yrkeskoderespons.kt
+++ src/main/kotlin/no/nav/tag/innsynAareg/models/Yrkeskoderespons/Yrkeskoderespons.kt
@@ -4,19 +4,11 @@ import com.fasterxml.jackson.annotation.JsonIgnoreProperties
 import lombok.AllArgsConstructor
 import lombok.Data
 import lombok.NoArgsConstructor
-import java.util.*
 
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
 @JsonIgnoreProperties(ignoreUnknown = true)
 class Yrkeskoderespons {
-    private var betydninger: Map<String?, List<Yrkeskode?>?>? = null
-    fun getBetydninger(): Map<String?, List<Yrkeskode?>?>? {
-        return betydninger
-    }
-
-    fun setBetydninger(betydninger: Map<String?, List<Yrkeskode?>?>?) {
-        this.betydninger = LinkedHashMap(betydninger)
-    }
+    val betydninger = hashMapOf<String, List<Yrkeskode>>()
 }
