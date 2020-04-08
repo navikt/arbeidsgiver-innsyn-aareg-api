a389509820a6c0a008cdb5e294e84bd82c52fb06
Ibenholt <bendik.segrov.ibenholt@nav.no>
2020-04-08 13:43:37 +0200

    tar imot selvbetjeningstoken

--- src/main/kotlin/no/nav/tag/innsynAareg/controller/AaregController.kt
+++ src/main/kotlin/no/nav/tag/innsynAareg/controller/AaregController.kt
@@ -3,15 +3,20 @@ package no.nav.tag.innsynAareg.controller
 import no.nav.security.token.support.core.api.Protected
 import no.nav.tag.innsynAareg.models.OversiktOverArbeidsForhold
 import no.nav.tag.innsynAareg.service.AaregService
+import org.springframework.web.bind.annotation.CookieValue
 import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.RequestHeader
 import org.springframework.web.bind.annotation.RestController
 import org.springframework.web.client.RestTemplate
+import springfox.documentation.annotations.ApiIgnore
 
 @RestController
 @Protected
 class AaregController (val resttemplate: RestTemplate, val aaregService:AaregService) {
     @GetMapping(value= ["/arbeidsforhold"])
-    fun hentArbeidsforhold(): OversiktOverArbeidsForhold {
-        return aaregService.hentArbeidsforhold("132","13");
+    fun hentArbeidsforhold(@RequestHeader("orgnr") orgnr:String,
+                           @RequestHeader("jurenhet") juridiskEnhetOrgnr:String ,
+                           @ApiIgnore @CookieValue("selvbetjening-idtoken") idToken:String): OversiktOverArbeidsForhold {
+        return aaregService.hentArbeidsforhold(orgnr,juridiskEnhetOrgnr,idToken);
     }
 }
