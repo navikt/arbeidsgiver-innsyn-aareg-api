c492aae10ab8a1f8ad977e8f3f62597881f476a8
Ibenholt <bendik.segrov.ibenholt@nav.no>
2020-04-16 12:46:32 +0200

    legg til yrkeskode i arbeidsforhold

--- src/main/kotlin/no/nav/tag/innsynAareg/service/AaregService.kt
+++ src/main/kotlin/no/nav/tag/innsynAareg/service/AaregService.kt
@@ -1,7 +1,9 @@
 package no.nav.tag.innsynAareg.service
 
 import no.nav.tag.innsynAareg.models.OversiktOverArbeidsForhold
+import no.nav.tag.innsynAareg.models.Yrkeskoderespons.Yrkeskoderespons
 import no.nav.tag.innsynAareg.service.sts.STSClient
+import no.nav.tag.innsynAareg.service.yrkeskoder.YrkeskodeverkService
 import org.springframework.beans.factory.annotation.Value
 import org.springframework.http.*
 import org.springframework.stereotype.Service
@@ -9,7 +11,7 @@ import org.springframework.web.client.RestClientException
 import org.springframework.web.client.RestTemplate
 
 @Service
-class AaregService (val restTemplate: RestTemplate, val stsClient: STSClient){
+class AaregService (val restTemplate: RestTemplate, val stsClient: STSClient,val yrkeskodeverkService: YrkeskodeverkService){
     @Value("\${aareg.aaregArbeidsforhold}")
     lateinit var aaregArbeidsforholdUrl: String
 
@@ -40,6 +42,19 @@ class AaregService (val restTemplate: RestTemplate, val stsClient: STSClient){
         headers["Nav-Consumer-Token"] = stsClient.token?.access_token;
         return HttpEntity(headers)
     }
-
+    fun settYrkeskodebetydningPaAlleArbeidsforhold(arbeidsforholdOversikt: OversiktOverArbeidsForhold): OversiktOverArbeidsForhold? {
+        //val hentYrkerTimer: Timer = MetricsFactory.createTimer("DittNavArbeidsgiverApi.hentYrker").start()
+        val yrkeskodeBeskrivelser: Yrkeskoderespons = yrkeskodeverkService.hentBetydningerAvYrkeskoder()!!
+        for (arbeidsforhold in arbeidsforholdOversikt.arbeidsforholdoversikter) {
+            val yrkeskode: String = arbeidsforhold.yrke
+            val yrkeskodeBeskrivelse: String = finnYrkeskodebetydningPaYrke(yrkeskode, yrkeskodeBeskrivelser)!!
+            arbeidsforhold.yrkesbeskrivelse =yrkeskodeBeskrivelse
+        }
+      //  hentYrkerTimer.stop().report()
+        return arbeidsforholdOversikt
+    }
+    fun finnYrkeskodebetydningPaYrke(yrkeskodenokkel: String?, yrkeskoderespons: Yrkeskoderespons): String? {
+        return yrkeskoderespons.betydninger.get(yrkeskodenokkel)?.get(0)?.beskrivelser?.nb?.tekst
+    }
 
 }
