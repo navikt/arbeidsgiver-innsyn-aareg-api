8c9e5d5beab853553eac2e44a864b68fe4f8261c
Ibenholt <bendik.segrov.ibenholt@nav.no>
2020-04-20 14:45:49 +0200

    fiks sånn at testene kjører uten exceptions

--- src/test/kotlin/no/nav/tag/innsynAareg/service/pdl/PdlServiceTest.kt
+++ src/test/kotlin/no/nav/tag/innsynAareg/service/pdl/PdlServiceTest.kt
@@ -1,15 +1,13 @@
 package no.nav.tag.innsynAareg.service.pdl
 
-import no.nav.tag.innsynAareg.models.pdlPerson.Data
-import no.nav.tag.innsynAareg.models.pdlPerson.Error
-import no.nav.tag.innsynAareg.models.pdlPerson.Navn
-import no.nav.tag.innsynAareg.models.pdlPerson.PdlRespons
+import no.nav.tag.innsynAareg.models.pdlPerson.*
 import no.nav.tag.innsynAareg.service.sts.STSClient
 import no.nav.tag.innsynAareg.utils.GraphQlUtils
 import org.apache.http.HttpEntity
 import org.junit.Assert
 import org.junit.Test
 import org.mockito.ArgumentMatchers
+import org.mockito.Matchers
 import org.mockito.Mockito
 import org.springframework.web.client.RestClientException
 import org.springframework.web.client.RestTemplate
@@ -18,12 +16,12 @@ import java.util.concurrent.ExecutionException
 
 class PdlServiceTest {
 
-    val mockPdlService = Mockito.mock(PdlService::class.java)
-    val stsClient = Mockito.mock(STSClient::class.java)
-    val restTemplate = Mockito.mock(RestTemplate::class.java)
-    val graphQlUtils = Mockito.mock(GraphQlUtils::class.java)
 
-    val mockRespons = lagPdlObjekt();
+    val mockStsClient = Mockito.mock(STSClient::class.java)
+    val mockRestTemplate = Mockito.mock(RestTemplate::class.java)
+    val graphQlUtils = Mockito.mock(GraphQlUtils::class.java)
+    val pdlService = PdlService(mockRestTemplate,mockStsClient,graphQlUtils,"http://test")
+    //val mockRespons = lagPdlObjekt();
 
     /*@SpringBootTest
     @TestPropertySource(properties = ["mock.port=8082"])
@@ -67,54 +65,57 @@ class PdlServiceTest {
         */
 
     fun lagPdlObjekt(): PdlRespons {
-        val respons = lagPdlObjekt();
-        val testNavn = Mockito.mock(Navn::class.java)
+        var respons = PdlRespons()
+        var testNavn = Navn()
         testNavn.fornavn = "Ole"
         testNavn.etternavn = "Dole"
-        respons.data = Mockito.mock(Data::class.java)
-        respons.data?.hentPerson?.navn = arrayOf(testNavn);
-        return respons;
+        respons.data = Data()
+        respons.data!!.hentPerson = HentPerson()
+        respons.data!!.hentPerson!!.navn = arrayOf(testNavn)
+        return respons
     }
 
-    @Test(expected = InterruptedException::class)
+    @Test
     fun hentNavnMedFnr_skal_hente_sts_token_og_returnere_navn_på_person() {
+        val respons = lagPdlObjekt();
         val navn = "Ole Dole"
-        Assert.assertEquals(navn, mockPdlService?.hentNavnMedFnr(FNR));
-        Mockito.verify<Any?>(stsClient.token?.access_token)
+        Mockito.`when`(mockRestTemplate.postForObject(Matchers.eq(PDL_URL), Matchers.any(org.springframework.http.HttpEntity::class.java), Matchers.eq(PdlRespons::class.java)))
+                .thenReturn(respons)
+        Assert.assertEquals(navn, pdlService.hentNavnMedFnr(FNR));
+       // Mockito.verify<Any?>(mockStsClient.token?.access_token)
     }
 
 
     @Test
-    @Throws(ExecutionException::class, InterruptedException::class)
     fun hentNavnMedFnr_skal_hente_sts_token_og_returnere_ikke_funnet_person() {
-        val tomRespons = PdlRespons()
+        var tomRespons = PdlRespons()
         val ingenPersonError = Error("Fant ikke Person");
         tomRespons.data = Data()
-        tomRespons.data?.hentPerson = null
+        tomRespons.data!!.hentPerson = null
         tomRespons.errors = ArrayList<Error>()
-        tomRespons.errors?.add(ingenPersonError)
-        Mockito.`when`(restTemplate?.postForObject(ArgumentMatchers.eq(PDL_URL), ArgumentMatchers.any(HttpEntity::class.java), ArgumentMatchers.eq(PdlRespons::class.java)))
+        tomRespons.errors!!.add(ingenPersonError)
+        Mockito.`when`(mockRestTemplate.postForObject(Matchers.eq(PDL_URL), Matchers.any(org.springframework.http.HttpEntity::class.java), Matchers.eq(PdlRespons::class.java)))
                 .thenReturn(tomRespons)
-        Assert.assertEquals("Kunne ikke hente navn", mockPdlService!!.hentNavnMedFnr(FNR))
-        Mockito.verify<Any?>(stsClient?.token?.access_token)
+        Assert.assertEquals("Kunne ikke hente navn", pdlService.hentNavnMedFnr(FNR))
+       // Mockito.verify<Any?>(mockStsClient?.token?.access_token)
     }
 
     @Test
     @Throws(ExecutionException::class, InterruptedException::class)
     fun hentNavnMedFnr_skal_hente_sts_token_og_returnere_ikke_funnet_person_v_helt_tomPdlRespons() {
         val tomRespons = PdlRespons()
-        Mockito.`when`(restTemplate?.postForObject(ArgumentMatchers.eq(PDL_URL), ArgumentMatchers.any(HttpEntity::class.java), ArgumentMatchers.eq(PdlRespons::class.java)))
+        Mockito.`when`(mockRestTemplate.postForObject(Matchers.eq(PDL_URL), Matchers.any(org.springframework.http.HttpEntity::class.java), Matchers.eq(PdlRespons::class.java)))
                 .thenReturn(tomRespons)
-        Assert.assertEquals("Kunne ikke hente navn", mockPdlService!!.hentNavnMedFnr(FNR))
-        Mockito.verify<Any?>(stsClient?.token?.access_token)
+        Assert.assertEquals("Kunne ikke hente navn", pdlService.hentNavnMedFnr(FNR))
+        //Mockito.verify<Any?>(mockStsClient?.token?.access_token)
     }
 
     @Test
     @Throws(ExecutionException::class, InterruptedException::class)
     fun hentNavnMedFnr_skal_hente_sts_token_fange_opp_feil() {
-        Mockito.`when`(restTemplate?.postForObject(ArgumentMatchers.eq(PDL_URL), ArgumentMatchers.any(HttpEntity::class.java), ArgumentMatchers.eq(PdlRespons::class.java))).thenThrow(RestClientException("401"))
-        Assert.assertEquals("Kunne ikke hente navn", mockPdlService!!.hentNavnMedFnr(FNR))
-        Mockito.verify<Any?>(stsClient?.token?.access_token)
+        Mockito.`when`(mockRestTemplate.postForObject(Matchers.eq(PDL_URL), Matchers.any(org.springframework.http.HttpEntity::class.java), Matchers.eq(PdlRespons::class.java))).thenThrow(RestClientException("401"))
+        Assert.assertEquals("Kunne ikke hente navn", pdlService!!.hentNavnMedFnr(FNR))
+        //Mockito.verify<Any?>(mockStsClient?.token?.access_token)
     }
 
     companion object {
