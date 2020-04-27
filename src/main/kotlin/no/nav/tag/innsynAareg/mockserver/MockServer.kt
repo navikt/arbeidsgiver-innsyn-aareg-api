b05022217fd2ec59dbb42ca4f780cb8c8bf0b69d
Hilde Steinbru Heggstad <hilde.steinbru.heggstad@nav.no>
2020-04-27 16:05:16 +0200

    fatt inn flere mockingfiler

--- src/main/kotlin/no/nav/tag/innsynAareg/mockserver/MockServer.kt
+++ src/main/kotlin/no/nav/tag/innsynAareg/mockserver/MockServer.kt
@@ -14,22 +14,26 @@ import java.nio.charset.StandardCharsets
 
 @Profile("local")
 @Component
-class MockServer @Autowired constructor(@Value("\${mock.port}")  val port: Int, @Value("\${sts.stsUrl}") val stsUrl: String, @Value("\${aareg.aaregArbeidsforhold}") val aaregArbeidsforholdUrl: String, @Value("\${yrkeskodeverk.yrkeskodeUrl}") val yrkeskodeUrl: String, @Value("\${pdl.pdlUrl}") val pdlUrl: String,@Value("\${ereg.url}") val eregUrl: String
+class MockServer @Autowired constructor(@Value("\${mock.port}")  val port: Int, @Value("\${sts.stsUrl}") val stsUrl: String, @Value("\${aareg.aaregArbeidsforhold}") val aaregArbeidsforholdUrl: String, @Value("\${aareg.aaregArbeidsgivere}") val aaregArbeidsgiveredUrl: String, @Value("\${yrkeskodeverk.yrkeskodeUrl}") val yrkeskodeUrl: String, @Value("\${pdl.pdlUrl}") val pdlUrl: String,@Value("\${ereg.url}") val eregUrl: String
 ) {
 
     init {
         System.out.println("mocking")
         val server = WireMockServer(WireMockConfiguration().port(port).extensions(ResponseTemplateTransformer(true)))
-        val aaregArbeidsforholdPath = URL(aaregArbeidsforholdUrl).path
-         mockForPath(server,aaregArbeidsforholdPath,"arbeidsforholdrespons.json")
+        val aaregArbeidsforholdPath = URL(aaregArbeidsforholdUrl).path;
+        mockForPath(server, aaregArbeidsforholdPath, "tomRespons.json")
+        val aaregArbeidsgiverePath = URL(aaregArbeidsgiveredUrl).path;
+        mockForPath(server, aaregArbeidsgiverePath, "arbeidsgiveroversiktaareg.json")
         val stsPath = URL(stsUrl).path
         mockForPath(server,stsPath,"STStoken.json")
         val yrkeskodePath = URL(yrkeskodeUrl).path
         mockForPath(server,yrkeskodePath,"yrkeskoder.json")
         val pdlPath = URL(pdlUrl).path
         mockForPath(server,pdlPath,"pdlRespons.json")
-        val eregPath= URL(eregUrl).path
-        mockForPath(server,eregPath,"enhetsregisteret.json")
+        val eregPath1= URL(eregUrl+ "910825518").path
+        val eregPath2= URL(eregUrl+ "910825517").path
+        mockForPath(server,eregPath1, "enhetsregisteret.json")
+        mockForPath(server,eregPath2, "enhetsregisteret.json")
 
         server.start()
     }
