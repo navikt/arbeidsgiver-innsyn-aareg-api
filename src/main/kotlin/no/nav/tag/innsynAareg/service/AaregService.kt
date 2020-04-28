67bb0ed0474191dea9b02fb394a7f4d77ceb5e5f
Hilde Steinbru Heggstad <hilde.steinbru.heggstad@nav.no>
2020-04-28 13:19:38 +0200

    omstrukturerrt aaregservice

--- src/main/kotlin/no/nav/tag/innsynAareg/service/AaregService.kt
+++ src/main/kotlin/no/nav/tag/innsynAareg/service/AaregService.kt
@@ -5,6 +5,9 @@ import no.nav.metrics.MetricsFactory
 import no.nav.metrics.Timer
 import no.nav.tag.innsynAareg.models.OversiktOverArbeidsForhold
 import no.nav.tag.innsynAareg.models.Yrkeskoderespons.Yrkeskoderespons
+import no.nav.tag.innsynAareg.models.enhetsregisteret.EnhetsRegisterOrg
+import no.nav.tag.innsynAareg.models.enhetsregisteret.Organisasjoneledd
+import no.nav.tag.innsynAareg.service.enhetsregisteret.EnhetsregisterService
 import no.nav.tag.innsynAareg.service.pdl.PdlService
 import no.nav.tag.innsynAareg.service.sts.STSClient
 import no.nav.tag.innsynAareg.service.yrkeskoder.YrkeskodeverkService
@@ -17,16 +20,19 @@ import org.springframework.web.client.RestTemplate
 
 @Slf4j
 @Service
-class AaregService (val restTemplate: RestTemplate, val stsClient: STSClient,val yrkeskodeverkService: YrkeskodeverkService ,val pdlService: PdlService){
+class AaregService (val restTemplate: RestTemplate, val stsClient: STSClient,val yrkeskodeverkService: YrkeskodeverkService ,val pdlService: PdlService, val enhetsregisteretService: EnhetsregisterService){
     @Value("\${aareg.aaregArbeidsforhold}")
     lateinit var aaregArbeidsforholdUrl: String
     val logger = LoggerFactory.getLogger(YrkeskodeverkService::class.java)
+
     fun hentArbeidsforhold(bedriftsnr:String, overOrdnetEnhetOrgnr:String?,idPortenToken: String?):OversiktOverArbeidsForhold {
         //val kunArbeidstimer: Timer= MetricsFactory.createTimer("DittNavArbeidsgiverApi.kunArbeidsforhold").start()
         val arbeidsforhold = hentArbeidsforholdFraAAReg(bedriftsnr,overOrdnetEnhetOrgnr,idPortenToken)
         //kunArbeidstimer.stop().report()
-        val arbeidsforholdMedNavn = settNavnPaArbeidsforhold(arbeidsforhold);
-        return settYrkeskodebetydningPaAlleArbeidsforhold(arbeidsforholdMedNavn!!)!!
+        if (arbeidsforhold.arbeidsforholdoversikter.isNullOrEmpty()) {
+            return settPaNavnOgYrkesbeskrivelse(finnOpplysningspliktigorg(bedriftsnr, idPortenToken!!)!!)!!
+        }
+        return settPaNavnOgYrkesbeskrivelse(arbeidsforhold)!!;
     }
 
     fun hentArbeidsforholdFraAAReg(bedriftsnr:String, overOrdnetEnhetOrgnr:String?,idPortenToken: String?):OversiktOverArbeidsForhold {
@@ -45,6 +51,33 @@ class AaregService (val restTemplate: RestTemplate, val stsClient: STSClient,val
         }
     }
 
+    fun finnOpplysningspliktigorg(orgnr: String, idToken: String?): OversiktOverArbeidsForhold? {
+        val orgtreFraEnhetsregisteret: EnhetsRegisterOrg? = enhetsregisteretService.hentOrgnaisasjonFraEnhetsregisteret(orgnr)
+        //no.nav.tag.dittNavArbeidsgiver.controller.AAregController.log.info("MSA-AAREG finnOpplysningspliktigorg, orgtreFraEnhetsregisteret: $orgtreFraEnhetsregisteret")
+        return if (!orgtreFraEnhetsregisteret?.bestaarAvOrganisasjonsledd.isNullOrEmpty()) {
+            itererOverOrgtre(orgnr, orgtreFraEnhetsregisteret!!.bestaarAvOrganisasjonsledd.get(0).organisasjonsledd!!, idToken)
+        } else return null;
+    }
+
+    fun itererOverOrgtre(orgnr: String, orgledd: Organisasjoneledd, idToken: String?): OversiktOverArbeidsForhold? {
+        val result: OversiktOverArbeidsForhold = hentArbeidsforholdFraAAReg(orgnr, orgledd!!.organisasjonsnummer, idToken)
+        //no.nav.tag.dittNavArbeidsgiver.controller.AAregController.log.info("MSA-AAREG itererOverOrgtre orgnr: " + orgnr + "orgledd: " + orgledd)
+        if (result.arbeidsforholdoversikter!!.isNotEmpty()) {
+            return result
+        } else if (orgledd.inngaarIJuridiskEnheter != null) {
+            val juridiskEnhetOrgnr: String? = orgledd.inngaarIJuridiskEnheter?.get(0)!!.organisasjonsnummer
+            //no.nav.tag.dittNavArbeidsgiver.controller.AAregController.log.info("MSA-AAREG itererOverOrgtre orgnr: " + orgnr + "juridiskEnhetOrgnr: " + juridiskEnhetOrgnr)
+            return hentArbeidsforholdFraAAReg(orgnr, juridiskEnhetOrgnr, idToken)
+        } else {
+            return itererOverOrgtre(orgnr, orgledd.organisasjonsleddOver!![0].organisasjonsledd!!, idToken)
+        }
+    }
+
+    fun settPaNavnOgYrkesbeskrivelse(arbeidsforhold :OversiktOverArbeidsForhold): OversiktOverArbeidsForhold?{
+        val arbeidsforholdMedNavn = settNavnPaArbeidsforhold(arbeidsforhold);
+        return settYrkeskodebetydningPaAlleArbeidsforhold(arbeidsforholdMedNavn!!)!!
+    }
+
     private fun getRequestEntity(bedriftsnr: String, juridiskEnhetOrgnr: String?, idPortenToken: String?): HttpEntity<String> {
         val appName = "srvditt-nav-arbeid"
         val headers = HttpHeaders()
@@ -56,6 +89,7 @@ class AaregService (val restTemplate: RestTemplate, val stsClient: STSClient,val
         headers["Nav-Consumer-Token"] = stsClient.token?.access_token;
         return HttpEntity(headers)
     }
+
     fun settYrkeskodebetydningPaAlleArbeidsforhold(arbeidsforholdOversikt: OversiktOverArbeidsForhold): OversiktOverArbeidsForhold? {
         val hentYrkerTimer: Timer = MetricsFactory.createTimer("DittNavArbeidsgiverApi.hentYrker").start()
         val yrkeskodeBeskrivelser: Yrkeskoderespons = yrkeskodeverkService.hentBetydningerAvYrkeskoder()!!
