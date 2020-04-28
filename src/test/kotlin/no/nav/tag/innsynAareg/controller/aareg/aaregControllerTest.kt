a49ed3cd34808d6a900430f844ab751bd1fc82ef
Hilde Steinbru Heggstad <hilde.steinbru.heggstad@nav.no>
2020-04-28 09:51:25 +0200

    en test passerer

--- src/test/kotlin/no/nav/tag/innsynAareg/controller/aareg/aaregControllerTest.kt
+++ src/test/kotlin/no/nav/tag/innsynAareg/controller/aareg/aaregControllerTest.kt
@@ -2,24 +2,14 @@ package no.nav.tag.innsynAareg.controller.aareg
 
 
 import no.nav.tag.innsynAareg.controller.AaregController
-import no.nav.tag.innsynAareg.service.AaregService
-import no.nav.tag.innsynAareg.service.enhetsregisteret.EnhetsregisterService
-import no.nav.tag.innsynAareg.service.pdl.PdlService
-import no.nav.tag.innsynAareg.service.sts.STSClient
-import no.nav.tag.innsynAareg.service.yrkeskoder.YrkeskodeverkService
 import org.junit.Assert
-import org.junit.Before
 import org.junit.Test
 import org.junit.runner.RunWith
-import org.mockito.InjectMocks
-import org.mockito.Mockito
-import org.mockito.MockitoAnnotations
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.test.context.SpringBootTest
 import org.springframework.test.context.ActiveProfiles
 import org.springframework.test.context.TestPropertySource
 import org.springframework.test.context.junit4.SpringRunner
-import org.springframework.web.client.RestTemplate
 
 
 @SpringBootTest
