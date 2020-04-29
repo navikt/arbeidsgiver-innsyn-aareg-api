a6f8dafdc74918ee48810d41628637eb2bd6c450
Hilde Steinbru Heggstad <hilde.steinbru.heggstad@nav.no>
2020-04-29 16:50:57 +0200

    skrevet om tester

--- src/main/kotlin/no/nav/tag/innsynAareg/models/OversiktOverArbeidgiver.kt
+++ src/main/kotlin/no/nav/tag/innsynAareg/models/OversiktOverArbeidgiver.kt
@@ -4,5 +4,5 @@ import com.fasterxml.jackson.annotation.JsonIgnoreProperties
 import com.fasterxml.jackson.annotation.JsonProperty
 
 @JsonIgnoreProperties(ignoreUnknown = true)
-data class OversiktOverArbeidsgiver (val arbeidsgiver: OversiktArbeidsgiver, val aktiveArbeidsforhold: Number,val inaktiveArbeidsforhold: Number )
+data class OversiktOverArbeidsgiver (val arbeidsgiver: OversiktArbeidsgiver, val aktiveArbeidsforhold: Int,val inaktiveArbeidsforhold: Int )
 data class OversiktArbeidsgiver(@JsonProperty("type") val type: String,val organisasjonsnummer: String )
