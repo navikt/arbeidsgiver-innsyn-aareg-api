819c69ca7a3daab04688893f5cfb2adbf96e37f0
Ibenholt <bendik.segrov.ibenholt@nav.no>
2020-05-05 15:01:35 +0200

    rettet feil hvor data ikke kunne være null i pdl respons

--- src/main/kotlin/no/nav/tag/innsynAareg/models/pdlPerson/PdlRespons.kt
+++ src/main/kotlin/no/nav/tag/innsynAareg/models/pdlPerson/PdlRespons.kt
@@ -3,4 +3,4 @@ package no.nav.tag.innsynAareg.models.pdlPerson
 
 import java.util.*
 
-data class PdlRespons (val data: Data,val errors:ArrayList<Error>?)
+data class PdlRespons (val data: Data?,val errors:ArrayList<Error>?)
