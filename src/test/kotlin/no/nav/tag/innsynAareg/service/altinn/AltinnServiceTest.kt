c6a2da3ce2b0fd421f6ce4cc28f9907bb736dade
Hilde Steinbru Heggstad <hilde.steinbru.heggstad@nav.no>
2020-05-13 11:28:24 +0200

    altinn test passerer

---
+++ src/test/kotlin/no/nav/tag/innsynAareg/service/altinn/AltinnServiceTest.kt
@@ -0,0 +1,38 @@
+package no.nav.tag.innsynAareg.service.altinn
+
+import no.nav.tag.innsynAareg.models.altinn.Organisasjon
+import no.nav.tag.innsynAareg.models.altinn.Role
+import no.nav.tag.innsynAareg.utils.TokenUtils
+import org.junit.Test
+import org.mockito.Matchers
+import org.mockito.Mockito
+import org.springframework.core.ParameterizedTypeReference
+import org.springframework.http.HttpEntity
+import org.springframework.http.HttpHeaders
+import org.springframework.http.HttpMethod
+import org.springframework.http.ResponseEntity
+import org.springframework.test.context.ActiveProfiles
+import org.springframework.web.client.RestTemplate
+import java.util.*
+
+@ActiveProfiles("local")
+class AltinnServiceTest {
+    val restTemplate = Mockito.mock(RestTemplate::class.java)
+    val tokenUtils: TokenUtils = Mockito.mock(TokenUtils::class.java)
+    val altinnconfigMock = Mockito.mock(AltinnConfig::class.java)
+    val altinnService = AltinnService(altinnconfigMock, restTemplate, tokenUtils)
+
+    @Test
+    fun hentOrganisasjoner__skal_kalle_altinn_flere_ganger_ved_stor_respons() {
+        val refTilListetype = typeReference<List<Organisasjon>>();
+        val tomHeader = HttpHeaders();
+
+        Mockito.`when`<ResponseEntity<*>>(restTemplate.exchange(Matchers.any(String::class.java), Matchers.eq(HttpMethod.GET), Matchers.any(HttpEntity::class.java), Matchers.any(ParameterizedTypeReference::class.java)))
+                .thenReturn(ResponseEntity.ok(Arrays.asList<Any>(Organisasjon())))
+                .thenReturn(ResponseEntity.ok(emptyList<Any>()))
+        altinnService.getFromAltinn(refTilListetype, "http://blabla", 1,HttpEntity(tomHeader));
+        Mockito.verify(restTemplate, Mockito.times(1)).exchange(Matchers.endsWith("&\$top=1&\$skip=0"), Matchers.eq(HttpMethod.GET), Matchers.any(HttpEntity::class.java), Matchers.any(ParameterizedTypeReference::class.java))
+        Mockito.verify(restTemplate, Mockito.times(1)).exchange(Matchers.endsWith("&\$top=1&\$skip=1"), Matchers.eq(HttpMethod.GET), Matchers.any(HttpEntity::class.java), Matchers.any(ParameterizedTypeReference::class.java))
+        Mockito.verifyNoMoreInteractions(restTemplate)
+    }
+}
