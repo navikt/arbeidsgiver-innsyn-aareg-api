d19b68022a13e747648353971e0718c24ec64a12
Hilde Steinbru Heggstad <hilde.steinbru.heggstad@nav.no>
2020-05-13 13:31:34 +0200

    skrevet om tester

---
+++ src/test/kotlin/no/nav/tag/innsynAareg/service/altinn/AltinnServiceIntegrationTest.kt
@@ -0,0 +1,53 @@
+package no.nav.tag.innsynAareg.service.altinn
+
+import no.nav.tag.innsynAareg.models.altinn.AltinnException
+import no.nav.tag.innsynAareg.models.altinn.Organisasjon
+import no.nav.tag.innsynAareg.models.altinn.Role
+import org.assertj.core.api.Assertions
+import org.junit.Assert
+import org.junit.Test
+import org.junit.runner.RunWith
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.boot.test.context.SpringBootTest
+import org.springframework.test.context.ActiveProfiles
+import org.springframework.test.context.TestPropertySource
+import org.springframework.test.context.junit4.SpringRunner
+
+const val SERVICE_EDITION = "1"
+const val SERVICE_CODE = "4936"
+const val FNR_MED_SKJEMATILGANG = "01065500791"
+const val FNR_MED_ORGANISASJONER = "00000000000"
+
+@RunWith(SpringRunner::class)
+@SpringBootTest
+@ActiveProfiles("dev")
+@TestPropertySource(properties = ["mock.port=8085"])
+class AltinnServiceIntegrationTest {
+    @Autowired
+    private val altinnService: AltinnService? = null
+
+    @Test
+    fun hentOrganisasjoner__skal_fungere_med_gyldig_fnr() {
+        val organisasjoner: List<Organisasjon> = altinnService!!.hentOrganisasjoner(FNR_MED_ORGANISASJONER)
+        Assert.assertEquals(5, organisasjoner.size);
+
+    }
+
+    @Test(expected = AltinnException::class)
+    fun hentOrganisasjoner__skal_kaste_altinn_exception_hvis_ugyldig_fnr() {
+        altinnService!!.hentOrganisasjoner("11111111111")
+    }
+
+    @Test
+    fun hentRoller__skal_fungere_med_gyldig_fnr_og_orgno() {
+        val roller: List<Role> = altinnService!!.hentRoller(FNR_MED_ORGANISASJONER, "000000000")
+        val testRolle = roller.find { it.RoleDescription == "Import, processing, production and/or sales of primary products and other foodstuff"}
+        Assert.assertEquals("Import, processing, production and/or sales of primary products and other foodstuff", testRolle!!.RoleDescription)
+    }
+
+    @Test
+    fun henttilgangTilSkjemForBedrift() {
+        val organisasjoner: List<Organisasjon> = altinnService!!.hentOrganisasjonerBasertPaRettigheter(FNR_MED_SKJEMATILGANG, SERVICE_CODE, SERVICE_EDITION)
+        Assert.assertEquals(5, organisasjoner.size);
+    }
+}
