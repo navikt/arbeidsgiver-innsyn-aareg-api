297323e95fa224eace70e43aa95d173f43fc6def
Ibenholt <bendik.segrov.ibenholt@nav.no>
2020-05-25 16:00:07 +0200

    fiks modell for altinnrespons

--- src/main/kotlin/no/nav/tag/innsynAareg/models/altinn/Organisasjon.kt
+++ src/main/kotlin/no/nav/tag/innsynAareg/models/altinn/Organisasjon.kt
@@ -1,15 +1,9 @@
 package no.nav.tag.innsynAareg.models.altinn
 
-import com.fasterxml.jackson.annotation.JsonIgnoreProperties
-import lombok.Data
-
-@JsonIgnoreProperties(ignoreUnknown = true)
-@Data
-class Organisasjon {
-    private val Name: String? = null
-    private val Type: String? = null
-    private val ParentOrganizationNumber: String? = null
-    private val OrganizationNumber: String? = null
-    private val OrganizationForm: String? = null
-    private val Status: String? = null
-}
+data class Organisasjon (val Name: String,
+                         val Type: String,
+                         val ParentOrganizationNumber: String?,
+                         val OrganizationNumber: String,
+                         val OrganizationForm: String,
+                         val Status: String
+    )
