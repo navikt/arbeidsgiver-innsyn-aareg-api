5d002b11d6918f885331c73686ed47b97a448327
Hilde Steinbru Heggstad <hilde.steinbru.heggstad@nav.no>
2020-06-25 10:14:32 +0200

    fiksa import

--- src/main/kotlin/no/nav/tag/innsynAareg/models/pdlBatch/PdlBatchRequest.kt
+++ src/main/kotlin/no/nav/tag/innsynAareg/models/pdlBatch/PdlBatchRequest.kt
@@ -3,4 +3,4 @@ package no.nav.tag.innsynAareg.models.pdlBatch
 import no.nav.tag.innsynAareg.models.pdlPerson.HentPerson
 import no.nav.tag.innsynAareg.models.pdlPerson.Variables
 
-data class PdlBatchRequest (val variables: Variables?, val query: String? = null)
+data class PdlBatchRequest ( val query: String? = null,val variables: Variables?)
