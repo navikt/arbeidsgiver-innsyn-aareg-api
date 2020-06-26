2c59535c60f8e0a6fe8e06ec21d7cd266bd6afe4
Hilde Steinbru Heggstad <hilde.steinbru.heggstad@nav.no>
2020-06-26 13:28:15 +0200

    lagd bedre feilhandtering

--- src/main/kotlin/no/nav/tag/innsynAareg/models/pdlBatch/PdlBatchRespons.kt
+++ src/main/kotlin/no/nav/tag/innsynAareg/models/pdlBatch/PdlBatchRespons.kt
@@ -2,4 +2,4 @@ package no.nav.tag.innsynAareg.models.pdlBatch
 import no.nav.tag.innsynAareg.models.pdlPerson.Error
 import java.util.*
 
-data class PdlBatchRespons (val data: Data?, val errors: ArrayList<Error?>?)
+data class PdlBatchRespons (val data: Data, val errors: ArrayList<Error?>?)
