857d41a7267e839beeecac1866d9e47c7d2fcce6
Hilde Steinbru Heggstad <hilde.steinbru.heggstad@nav.no>
2020-06-29 09:58:35 +0200

    fjernet gammel pdl

--- src/main/kotlin/no/nav/tag/innsynAareg/models/pdlBatch/PdlBatchRespons.kt
+++ src/main/kotlin/no/nav/tag/innsynAareg/models/pdlBatch/PdlBatchRespons.kt
@@ -1,5 +1,4 @@
 package no.nav.tag.innsynAareg.models.pdlBatch
-import no.nav.tag.innsynAareg.models.pdlPerson.Error
 import java.util.*
 
 data class PdlBatchRespons (val data: Data, val errors: ArrayList<Error?>?)
