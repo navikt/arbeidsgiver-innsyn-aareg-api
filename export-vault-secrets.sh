e67a097bb089440ec0f9d52648a73489bc79ecb7
Hilde Steinbru Heggstad <hilde.steinbru.heggstad@nav.no>
2020-07-01 16:33:55 +0200

    forsokt med case sensitivt i fila

--- export-vault-secrets.sh
+++ export-vault-secrets.sh
@@ -2,6 +2,6 @@
 
 if test -f /var/run/secrets/nais.io/service_user/password;
 then
-    export  STS_PASS=$(cat /var/run/secrets/nais.io/srvag-arbforhold/password)
+    export  STS_PASS=$(cat /var/run/secrets/nais.io/srvAG-Arbforhold/password)
     echo "Setting SYSTEMBRUKER_STS_PASS"
 fi
