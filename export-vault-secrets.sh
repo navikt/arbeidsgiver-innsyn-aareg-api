ac15eae3eebc71a9fe3b8b6af05ab8da68fcac59
Hilde Steinbru Heggstad <hilde.steinbru.heggstad@nav.no>
2020-07-02 09:26:51 +0200

    systembrukerfil

--- export-vault-secrets.sh
+++ export-vault-secrets.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
-if test -f /var/run/secrets/nais.io/service_user/password;
+if test -f /var/run/secrets/nais.io/srvag-arbforhold/password;
 then
-    export  STS_PASS=$(cat /var/run/secrets/nais.io/srvAG-Arbforhold/password)
+    export  STS_PASS=$(cat /var/run/secrets/nais.io/srvag-arbforhold/password)
     echo "Setting SYSTEMBRUKER_STS_PASS"
 fi
