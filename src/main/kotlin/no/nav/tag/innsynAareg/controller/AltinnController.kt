b7cf4d900f82e1a88e0dd7c713ac4e8d703bebba
hilde.heggstad <hilde.heggstad@bekk.no>
2020-07-30 11:56:44 +0200

    fikser bygg

--- src/main/kotlin/no/nav/tag/innsynAareg/controller/AltinnController.kt
+++ src/main/kotlin/no/nav/tag/innsynAareg/controller/AltinnController.kt
@@ -10,7 +10,6 @@ import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.RequestParam
 import org.springframework.web.bind.annotation.RestController
 import no.nav.security.token.support.core.context.TokenValidationContextHolder
-import no.nav.tag.innsynAareg.service.altinn.typeReference
 
 @Protected
 @Slf4j
