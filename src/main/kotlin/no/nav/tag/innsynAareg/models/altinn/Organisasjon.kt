6685529b0c3d06cdcdf0c55c109c95c31f816a27
hilde.heggstad <hilde.heggstad@bekk.no>
2020-08-03 11:02:18 +0200

    lagd tom konstruktor for a lage ny org

--- src/main/kotlin/no/nav/tag/innsynAareg/models/altinn/Organisasjon.kt
+++ src/main/kotlin/no/nav/tag/innsynAareg/models/altinn/Organisasjon.kt
@@ -1,9 +1,18 @@
 package no.nav.tag.innsynAareg.models.altinn
 
 data class Organisasjon (var Name: String?,
-                         val Type: String?,
-                         val ParentOrganizationNumber: String?,
-                         val OrganizationNumber: String?,
-                         val OrganizationForm: String?,
-                         val Status: String?
-    )
+                         var Type: String?,
+                         var ParentOrganizationNumber: String?,
+                         var OrganizationNumber: String?,
+                         var OrganizationForm: String?,
+                         var Status: String?
+    ) {
+    constructor() {
+        Name = null;
+        Type = null;
+        ParentOrganizationNumber = null;
+        OrganizationForm = null;
+        Status = null;
+
+    }
+}
