6efbcfa0c60de17710cf7f6a43dc8d8f9f5f4021
Peter Brottveit Bock <peter.brottveit.bock@nav.no>
2020-08-17 12:15:52 +0200

    Hent tilbake DTOene for frontend

--- src/main/kotlin/no/nav/tag/innsynAareg/client/aareg/dto/OversiktOverArbeidsForhold.kt
+++ src/main/kotlin/no/nav/tag/innsynAareg/client/aareg/dto/OversiktOverArbeidsForhold.kt
@@ -1,25 +1,62 @@
 package no.nav.tag.innsynAareg.client.aareg.dto
 
-import com.fasterxml.jackson.annotation.JsonIgnoreProperties
+val varselKodeOppslag: HashMap<String, String> = hashMapOf(
+    "ERKONK" to "Maskinell sluttdato: Konkurs",
+    "EROPPH" to "Maskinell sluttdato: Opphørt i Enhetsregisteret",
+    "ERVIRK" to "Maskinell sluttdato: Virksomhetoverdragelse",
+    "IBARBG" to "Maskinell sluttdato: Ikke bekreftet",
+    "IBKAOR" to "Maskinell sluttdato: Ikke bekreftet i a-ordningen"
+)
 
+data class Arbeidsgiver(
+    val type: String
+)
 
-@JsonIgnoreProperties(ignoreUnknown = true)
 data class Arbeidstaker(
+    val type: String,
+    val aktoerId: String,
     val offentligIdent: String,
     var navn: String?
 )
 
-@JsonIgnoreProperties(ignoreUnknown = true)
+data class Opplysningspliktig(
+    val type: String
+)
+
+data class ArbeidsforholdVarsel(
+    val entitet: String
+) {
+    var varslingskode: String? = null
+        set(value) {
+            field = value
+            this.varslingskodeForklaring = varselKodeOppslag[varslingskode]
+        }
+
+    var varslingskodeForklaring: String? = null
+}
+
 data class ArbeidsForhold(
+    val ansattFom: String,
+    val ansattTom: String?,
+    val arbeidsgiver: Arbeidsgiver,
     var arbeidstaker: Arbeidstaker,
+    val innrapportertEtterAOrdningen: String,
+    val navArbeidsforholdId: String,
+    val opplysningspliktig: Opplysningspliktig,
+    val sistBekreftet: String,
+    val stillingsprosent: String,
+    val type: String,
+    val varsler: List<ArbeidsforholdVarsel>?,
     val yrke: String,
     var yrkesbeskrivelse: String?
 )
 
-@JsonIgnoreProperties(ignoreUnknown = true)
 data class OversiktOverArbeidsForhold(
     val antall: Long?,
     val arbeidsforholdoversikter: List<ArbeidsForhold>?,
     val startrad: String?,
     val totalAntall: String?
 )
+
+sealed class AaregFeil
+object IngenRettigheter : AaregFeil()
