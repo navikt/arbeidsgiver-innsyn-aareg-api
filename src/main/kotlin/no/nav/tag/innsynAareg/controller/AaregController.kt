b1a17de24703da2f428f1d4d918581d9f5b44dc0
Peter Brottveit Bock <peter.brottveit.bock@nav.no>
2020-08-19 09:41:11 +0200

    Propager 403/forbidden til frontend.

--- src/main/kotlin/no/nav/tag/innsynAareg/controller/AaregController.kt
+++ src/main/kotlin/no/nav/tag/innsynAareg/controller/AaregController.kt
@@ -2,7 +2,11 @@ package no.nav.tag.innsynAareg.controller
 
 import no.nav.security.token.support.core.api.Protected
 import no.nav.tag.innsynAareg.client.aareg.dto.OversiktOverArbeidsForhold
+import no.nav.tag.innsynAareg.models.ArbeidsforholdFunnet
+import no.nav.tag.innsynAareg.models.IngenRettigheter
 import no.nav.tag.innsynAareg.service.InnsynService
+import org.springframework.http.HttpStatus
+import org.springframework.http.ResponseEntity
 import org.springframework.web.bind.annotation.CookieValue
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.RequestHeader
@@ -17,9 +21,11 @@ class AaregController(val aAregService: InnsynService) {
         @RequestHeader("orgnr") orgnr: String,
         @RequestHeader("jurenhet") juridiskEnhetOrgnr: String,
         @ApiIgnore @CookieValue("selvbetjening-idtoken") idToken: String
-    ): OversiktOverArbeidsForhold {
-        return aAregService.hentArbeidsforhold(orgnr, juridiskEnhetOrgnr, idToken)
-    }
+    ): ResponseEntity<OversiktOverArbeidsForhold> =
+        when (val respons = aAregService.hentArbeidsforhold(orgnr, juridiskEnhetOrgnr, idToken)) {
+            is ArbeidsforholdFunnet -> ResponseEntity.ok(respons.oversiktOverArbeidsForhold)
+            IngenRettigheter -> ResponseEntity(HttpStatus.FORBIDDEN)
+        }
 
     @GetMapping(value = ["/antall-arbeidsforhold"])
     fun hentAntallArbeidsforhold(
