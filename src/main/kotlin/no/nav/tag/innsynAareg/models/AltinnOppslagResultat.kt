71f61cbbca606e2dae808606eb96d4e3f5c82755
Peter Brottveit Bock <peter.brottveit.bock@nav.no>
2020-08-20 14:35:09 +0200

    Forward 403 fra altinn-klient til frontend

---
+++ src/main/kotlin/no/nav/tag/innsynAareg/models/AltinnOppslagResultat.kt
@@ -0,0 +1,11 @@
+package no.nav.tag.innsynAareg.models
+
+import no.nav.tag.innsynAareg.client.altinn.dto.Organisasjon
+
+
+sealed class AltinnOppslagResultat
+
+data class AltinnOppslagVellykket(val organisasjoner: List<Organisasjon>) : AltinnOppslagResultat()
+
+object AltinnIngenRettigheter : AltinnOppslagResultat()
+
