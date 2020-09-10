686769df78ffdbd4dab8781cfc31f9e4c89889b7
Bendik Ibenholt <bendik.segrov.ibenholt@nav.no>
2020-09-10 09:43:19 +0300

    Featuretoggle proxy (#132)
    * unleash proxy med environment strategi
    
    * Legg til byuserid startegy
    
    * pump spring versjon
    
    * by environmentstrategy og tester
    
    * rydding

---
+++ src/main/kotlin/no/nav/tag/innsynAareg/service/featuretoggle/ByEnvironmentStrategy.kt
@@ -0,0 +1,24 @@
+package no.nav.tag.innsynAareg.service.featuretoggle
+
+import no.finn.unleash.strategy.Strategy
+import org.springframework.core.env.Environment
+import org.springframework.stereotype.Component
+
+@Component
+class ByEnvironmentStrategy(currentEnv: Environment) : Strategy {
+
+    val MILJOER = listOf("dev-fss", "prod-fss")
+    val currentEnv = currentEnv.activeProfiles.find { MILJOER.contains(it) } ?: "dev-fss"
+
+    override fun getName(): String {
+        return "byEnvironment"
+    }
+
+    override fun isEnabled(parameters: Map<String, String>?): Boolean {
+        return parameters?.get("miljø")?.split(',')?.any { isCurrentEnv(it) } ?: false
+    }
+
+    fun isCurrentEnv(env: String): Boolean {
+        return currentEnv == env
+    }
+}
---
+++ src/main/kotlin/no/nav/tag/innsynAareg/service/featuretoggle/UnleashService.kt
@@ -0,0 +1,26 @@
+package no.nav.tag.innsynAareg.service.featuretoggle
+
+import no.finn.unleash.Unleash
+import no.finn.unleash.UnleashContext
+import no.nav.tag.innsynAareg.utils.TokenUtils
+import org.springframework.stereotype.Service
+
+@Service
+class UnleashService(private val unleash: Unleash, tokenUtils: TokenUtils) {
+
+    private val tokenUtil: TokenUtils = tokenUtils
+
+    fun hentFeatureToggles(features: List<String>): Map<String, Boolean?>? {
+        return features.map { it to isEnabled(it) }.toMap()
+    }
+
+    fun isEnabled(feature: String?): Boolean? {
+        return unleash.isEnabled(feature, contextMedInnloggetBruker())
+    }
+
+    private fun contextMedInnloggetBruker(): UnleashContext? {
+        val builder = UnleashContext.builder()
+        builder.userId(tokenUtil.autentisertBruker())
+        return builder.build()
+    }
+}
