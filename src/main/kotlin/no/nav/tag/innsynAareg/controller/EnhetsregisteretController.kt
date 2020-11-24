ff259853d6c64f30b62869e75039de182dcc6fda
Hilde Steinbru Heggstad <hilde.steinbru.heggstad@nav.no>
2020-11-24 16:17:27 +0100

    satt pa tvunget niva 4 pa innloggings controller

--- src/main/kotlin/no/nav/tag/innsynAareg/controller/EnhetsregisteretController.kt
+++ src/main/kotlin/no/nav/tag/innsynAareg/controller/EnhetsregisteretController.kt
@@ -1,10 +1,8 @@
 package no.nav.tag.innsynAareg.controller
 
 import no.nav.security.token.support.core.context.TokenValidationContextHolder
-import no.nav.tag.innsynAareg.client.altinn.AltinnClient
 import no.nav.tag.innsynAareg.client.altinn.dto.Organisasjon
 import no.nav.tag.innsynAareg.client.enhetsregisteret.EnhetsregisteretClient
-import no.nav.tag.innsynAareg.utils.FnrExtractor
 import org.springframework.http.ResponseEntity
 
 import no.nav.security.token.support.core.api.Protected
--- src/main/kotlin/no/nav/tag/innsynAareg/controller/InnloggingsController.kt
+++ src/main/kotlin/no/nav/tag/innsynAareg/controller/InnloggingsController.kt
@@ -1,11 +1,11 @@
 package no.nav.tag.innsynAareg.controller
-import no.nav.security.token.support.core.api.Protected
+import no.nav.security.token.support.core.api.ProtectedWithClaims
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.ResponseBody
 import org.springframework.web.bind.annotation.RestController
 
 @RestController
-@Protected
+@ProtectedWithClaims(issuer="selvbetjening", claimMap= ["acr=Level4"])
 class InnloggingsController {
     @GetMapping(value = ["/innlogget"])
     @ResponseBody
