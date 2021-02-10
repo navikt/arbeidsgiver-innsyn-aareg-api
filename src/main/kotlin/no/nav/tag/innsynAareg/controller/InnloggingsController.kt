182029aaa3c6395e2473d7150f839b72c5740ece
Ibenholt <bendik.segrov.ibenholt@nav.no>
2021-02-10 14:52:34 +0100

    fjern endring av rettigheter i export-vault-secrets.sh

--- src/main/kotlin/no/nav/tag/innsynAareg/controller/InnloggingsController.kt
+++ src/main/kotlin/no/nav/tag/innsynAareg/controller/InnloggingsController.kt
@@ -7,6 +7,7 @@ import org.springframework.http.ResponseEntity
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.ResponseBody
 import org.springframework.web.bind.annotation.RestController
+import java.util.concurrent.TimeUnit
 
 @RestController
 @ProtectedWithClaims(issuer = ISSUER, claimMap = [LEVEL])
