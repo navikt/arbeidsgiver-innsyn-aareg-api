d6116d3c78a4a6a60dda5f82d884603093033b4c
Ibenholt <bendik.segrov.ibenholt@nav.no>
2021-02-10 14:26:20 +0100

    legg til no-store header

--- src/main/kotlin/no/nav/tag/innsynAareg/controller/InnloggingsController.kt
+++ src/main/kotlin/no/nav/tag/innsynAareg/controller/InnloggingsController.kt
@@ -2,16 +2,21 @@ package no.nav.tag.innsynAareg.controller
 import no.nav.security.token.support.core.api.ProtectedWithClaims
 import no.nav.tag.innsynAareg.utils.ISSUER
 import no.nav.tag.innsynAareg.utils.LEVEL
+import org.springframework.http.CacheControl
+import org.springframework.http.ResponseEntity
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.ResponseBody
 import org.springframework.web.bind.annotation.RestController
 
 @RestController
-@ProtectedWithClaims(issuer=ISSUER , claimMap= [LEVEL])
+@ProtectedWithClaims(issuer = ISSUER, claimMap = [LEVEL])
 class InnloggingsController {
     @GetMapping(value = ["/innlogget"])
     @ResponseBody
-    fun erInnlogget(): String? {
-        return "ok"
+    fun erInnlogget(): ResponseEntity<String> {
+        val cacheControl = CacheControl.noStore()
+        return ResponseEntity.ok()
+                .cacheControl(cacheControl)
+                .body("ok")
     }
 }
