3ae72968dadbc975792efa56d5c5ded0cfee049d
Ken Gullaksen <ken.gullaksen@nav.no>
2021-12-16 10:25:56 +0100

    legg til test som beviser feil med inlogging og 1.3.9 token-support

---
+++ src/test/kotlin/no/nav/tag/innsynAareg/controller/InnloggingsControllerTest.kt
@@ -0,0 +1,42 @@
+package no.nav.tag.innsynAareg.controller
+
+import org.junit.Test
+import org.junit.runner.RunWith
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc
+import org.springframework.boot.test.context.SpringBootTest
+import org.springframework.http.HttpHeaders.AUTHORIZATION
+import org.springframework.test.context.ActiveProfiles
+import org.springframework.test.context.TestPropertySource
+import org.springframework.test.context.junit4.SpringRunner
+import org.springframework.test.web.servlet.MockMvc
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers.status
+
+@SpringBootTest
+@AutoConfigureMockMvc
+@RunWith(SpringRunner::class)
+@ActiveProfiles("local")
+@TestPropertySource(properties = ["mock.port=8082", "logging.level.root=DEBUG"])
+class InnloggingsControllerTest {
+    @Autowired
+    lateinit var mockMvc: MockMvc
+
+    @Test
+    fun `returnerer 401 når request ikke er innlogget`() {
+        mockMvc
+            .perform(get("/innlogget"))
+            .andExpect(status().isUnauthorized)
+    }
+
+    @Test
+    fun `returnerer 200 når request er innlogget`() {
+        val jwt = mockMvc.perform(get("/local/jwt")).andReturn().response.contentAsString
+        mockMvc
+            .perform(
+                get("/innlogget")
+                    .header(AUTHORIZATION, "Bearer $jwt")
+            )
+            .andExpect(status().isOk)
+    }
+}
