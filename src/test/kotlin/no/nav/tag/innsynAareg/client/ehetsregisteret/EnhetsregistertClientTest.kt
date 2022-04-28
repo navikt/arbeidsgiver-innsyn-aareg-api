eba39df8543a528a2b2c176d92e97b24d530dd38
Ken Gullaksen <ken.gullaksen@nav.no>
2022-04-28 08:27:08 +0200

    fiks tester

--- src/test/kotlin/no/nav/tag/innsynAareg/client/ehetsregisteret/EnhetsregistertClientTest.kt
+++ src/test/kotlin/no/nav/tag/innsynAareg/client/ehetsregisteret/EnhetsregistertClientTest.kt
@@ -1,16 +1,19 @@
 package no.nav.tag.innsynAareg.client.ehetsregisteret
 
+import no.nav.security.token.support.spring.test.EnableMockOAuth2Server
 import no.nav.tag.innsynAareg.client.enhetsregisteret.EnhetsregisteretClient
 import org.junit.Assert
 import org.junit.Test
 import org.junit.runner.RunWith
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.test.context.SpringBootTest
+import org.springframework.boot.test.context.SpringBootTest.WebEnvironment.RANDOM_PORT
 import org.springframework.test.context.ActiveProfiles
 import org.springframework.test.context.TestPropertySource
 import org.springframework.test.context.junit4.SpringRunner
 
-@SpringBootTest
+@SpringBootTest(webEnvironment = RANDOM_PORT)
+@EnableMockOAuth2Server
 @RunWith(SpringRunner::class)
 @ActiveProfiles("local")
 @TestPropertySource(properties = ["mock.port=8082"])
@@ -22,6 +25,6 @@ class EnhetsregistertClientTest {
     fun getToken() {
         val result = enhetsregisteretClient.finnTidligereVirksomheter("910825518", "123")
         Assert.assertNotNull (result)
-        Assert.assertEquals(2,result?.size)
+        Assert.assertEquals(2, result.size)
     }
 }
