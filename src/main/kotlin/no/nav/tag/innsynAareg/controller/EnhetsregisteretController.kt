34694530122fb52bcc2245f5d79ea63a24e42842
ebelegu <egzon.belegu@nav.no>
2022-09-28 09:35:20 +0200

    Fjernet unødvendig token-parameter

--- src/main/kotlin/no/nav/tag/innsynAareg/controller/EnhetsregisteretController.kt
+++ src/main/kotlin/no/nav/tag/innsynAareg/controller/EnhetsregisteretController.kt
@@ -1,13 +1,10 @@
 package no.nav.tag.innsynAareg.controller
-
-import io.swagger.v3.oas.annotations.Parameter
 import no.nav.security.token.support.core.api.ProtectedWithClaims
 import no.nav.tag.innsynAareg.client.altinn.dto.Organisasjon
 import no.nav.tag.innsynAareg.client.enhetsregisteret.EnhetsregisteretClient
 import no.nav.tag.innsynAareg.utils.ISSUER
 import no.nav.tag.innsynAareg.utils.LEVEL
 import org.springframework.http.ResponseEntity
-import org.springframework.web.bind.annotation.CookieValue
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.RequestHeader
 import org.springframework.web.bind.annotation.RestController
@@ -20,9 +17,8 @@ class EnhetsregisteretController(
     @GetMapping(value = ["/tidligere-virksomheter"])
     fun hentTidligerVirksomheter(
         @RequestHeader("jurenhet") juridiskEnhetOrgnr: String,
-        @Parameter(hidden = true) @CookieValue("selvbetjening-idtoken") idToken: String
     ): ResponseEntity<List<Organisasjon>> {
-        val result = enhetsregisteretClient.finnTidligereVirksomheter(juridiskEnhetOrgnr, idToken)
+        val result = enhetsregisteretClient.finnTidligereVirksomheter(juridiskEnhetOrgnr)
         return ResponseEntity.ok(result)
     }
 }
