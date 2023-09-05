5162d769d9b657d1cc6d7f56073cbcead50b4457
Ken Gullaksen <ken.gullaksen@nav.no>
2023-09-05 10:06:15 +0200

    støtt ny og gammel claim for nivå 4 (#407)
    ref: https://nav-it.slack.com/archives/C01DE3M9YBV/p1686121200932109

--- src/main/kotlin/no/nav/tag/innsynAareg/controller/AltinnController.kt
+++ src/main/kotlin/no/nav/tag/innsynAareg/controller/AltinnController.kt
@@ -5,16 +5,21 @@ import no.nav.tag.innsynAareg.client.altinn.AltinnClient
 import no.nav.tag.innsynAareg.client.altinn.dto.Organisasjon
 import no.nav.tag.innsynAareg.models.AltinnIngenRettigheter
 import no.nav.tag.innsynAareg.models.AltinnOppslagVellykket
+import no.nav.tag.innsynAareg.utils.ACR_CLAIM_NEW
 import no.nav.tag.innsynAareg.utils.AutentisertBruker
 import no.nav.tag.innsynAareg.utils.ISSUER
-import no.nav.tag.innsynAareg.utils.LEVEL
+import no.nav.tag.innsynAareg.utils.ACR_CLAIM_OLD
 import org.springframework.http.HttpStatus
 import org.springframework.http.ResponseEntity
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.RequestParam
 import org.springframework.web.bind.annotation.RestController
 
-@ProtectedWithClaims(issuer = ISSUER, claimMap = [LEVEL])
+@ProtectedWithClaims(
+    issuer = ISSUER,
+    claimMap = [ACR_CLAIM_OLD, ACR_CLAIM_NEW],
+    combineWithOr = true,
+)
 @RestController
 class OrganisasjonController(
     val altinnClient: AltinnClient,
