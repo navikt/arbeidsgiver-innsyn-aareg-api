9253679c38df10bc4d9f22d9cec5f518cbbfa722
Ken Gullaksen <ken.gullaksen@nav.no>
2023-09-05 10:06:15 +0200

    støtt ny og gammel claim for nivå 4 (#407)
    ref: https://nav-it.slack.com/archives/C01DE3M9YBV/p1686121200932109

--- src/main/kotlin/no/nav/tag/innsynAareg/controller/EnhetsregisteretController.kt
+++ src/main/kotlin/no/nav/tag/innsynAareg/controller/EnhetsregisteretController.kt
@@ -2,15 +2,20 @@ package no.nav.tag.innsynAareg.controller
 import no.nav.security.token.support.core.api.ProtectedWithClaims
 import no.nav.tag.innsynAareg.client.altinn.dto.Organisasjon
 import no.nav.tag.innsynAareg.client.enhetsregisteret.EnhetsregisteretClient
+import no.nav.tag.innsynAareg.utils.ACR_CLAIM_NEW
 import no.nav.tag.innsynAareg.utils.ISSUER
-import no.nav.tag.innsynAareg.utils.LEVEL
+import no.nav.tag.innsynAareg.utils.ACR_CLAIM_OLD
 import org.springframework.http.ResponseEntity
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.RequestHeader
 import org.springframework.web.bind.annotation.RestController
 
 @RestController
-@ProtectedWithClaims(issuer = ISSUER, claimMap = [LEVEL])
+@ProtectedWithClaims(
+    issuer = ISSUER,
+    claimMap = [ACR_CLAIM_OLD, ACR_CLAIM_NEW],
+    combineWithOr = true,
+)
 class EnhetsregisteretController(
     val enhetsregisteretClient: EnhetsregisteretClient
 ) {
--- src/main/kotlin/no/nav/tag/innsynAareg/controller/InnloggingsController.kt
+++ src/main/kotlin/no/nav/tag/innsynAareg/controller/InnloggingsController.kt
@@ -1,7 +1,8 @@
 package no.nav.tag.innsynAareg.controller
 import no.nav.security.token.support.core.api.ProtectedWithClaims
+import no.nav.tag.innsynAareg.utils.ACR_CLAIM_NEW
 import no.nav.tag.innsynAareg.utils.ISSUER
-import no.nav.tag.innsynAareg.utils.LEVEL
+import no.nav.tag.innsynAareg.utils.ACR_CLAIM_OLD
 import org.springframework.http.CacheControl
 import org.springframework.http.ResponseEntity
 import org.springframework.web.bind.annotation.GetMapping
@@ -9,7 +10,11 @@ import org.springframework.web.bind.annotation.ResponseBody
 import org.springframework.web.bind.annotation.RestController
 
 @RestController
-@ProtectedWithClaims(issuer = ISSUER, claimMap = [LEVEL])
+@ProtectedWithClaims(
+    issuer = ISSUER,
+    claimMap = [ACR_CLAIM_OLD, ACR_CLAIM_NEW],
+    combineWithOr = true,
+)
 class InnloggingsController {
     @GetMapping(value = ["/innlogget"])
     @ResponseBody
