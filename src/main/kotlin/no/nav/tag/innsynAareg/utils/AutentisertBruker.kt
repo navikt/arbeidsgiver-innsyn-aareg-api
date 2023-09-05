a8ec1ff3bcde47a702b149c851b3508b5f1a4cd6
Ken Gullaksen <ken.gullaksen@nav.no>
2023-09-05 11:08:19 +0200

    bruk tokenx i tokensupport (#409)

--- src/main/kotlin/no/nav/tag/innsynAareg/utils/AutentisertBruker.kt
+++ src/main/kotlin/no/nav/tag/innsynAareg/utils/AutentisertBruker.kt
@@ -6,7 +6,7 @@ import org.springframework.stereotype.Component
 
 const val ACR_CLAIM_OLD = "acr=Level4"
 const val ACR_CLAIM_NEW = "acr=idporten-loa-high"
-const val ISSUER = "selvbetjening"
+const val ISSUER = "tokenx"
 
 @Component
 class AutentisertBruker(
