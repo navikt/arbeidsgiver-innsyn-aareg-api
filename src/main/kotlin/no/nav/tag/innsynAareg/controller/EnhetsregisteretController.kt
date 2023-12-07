f703e81d5ddfc7f86746891f072a74c2804205af
Knut Arild Slåtsve <53740375+KnutArildSlaatsve@users.noreply.github.com>
2023-12-07 06:59:23 +0100

    Fjernet IDPorten level 4 acr claim (#412)

--- src/main/kotlin/no/nav/tag/innsynAareg/controller/EnhetsregisteretController.kt
+++ src/main/kotlin/no/nav/tag/innsynAareg/controller/EnhetsregisteretController.kt
@@ -4,7 +4,6 @@ import no.nav.tag.innsynAareg.client.altinn.dto.Organisasjon
 import no.nav.tag.innsynAareg.client.enhetsregisteret.EnhetsregisteretClient
 import no.nav.tag.innsynAareg.utils.ACR_CLAIM_NEW
 import no.nav.tag.innsynAareg.utils.ISSUER
-import no.nav.tag.innsynAareg.utils.ACR_CLAIM_OLD
 import org.springframework.http.ResponseEntity
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.RequestHeader
@@ -13,7 +12,7 @@ import org.springframework.web.bind.annotation.RestController
 @RestController
 @ProtectedWithClaims(
     issuer = ISSUER,
-    claimMap = [ACR_CLAIM_OLD, ACR_CLAIM_NEW],
+    claimMap = [ACR_CLAIM_NEW],
     combineWithOr = true,
 )
 class EnhetsregisteretController(
