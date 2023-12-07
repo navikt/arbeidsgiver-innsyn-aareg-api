f489cb2c1eee0e7d7050d53530b1d13c94821f05
Knut Arild Slåtsve <knut.arild.slatsve@nav.no>
2023-12-07 09:06:17 +0100

    Revert "Fjernet IDPorten level 4 acr claim (#412)"
    This reverts commit 2f8b615c7fefdb5d462c49476c8a3298fc07eddf.

--- src/main/kotlin/no/nav/tag/innsynAareg/utils/AutentisertBruker.kt
+++ src/main/kotlin/no/nav/tag/innsynAareg/utils/AutentisertBruker.kt
@@ -4,6 +4,7 @@ import no.nav.security.token.support.core.context.TokenValidationContextHolder
 import no.nav.security.token.support.core.jwt.JwtToken
 import org.springframework.stereotype.Component
 
+const val ACR_CLAIM_OLD = "acr=Level4"
 const val ACR_CLAIM_NEW = "acr=idporten-loa-high"
 const val ISSUER = "tokenx"
 
