dbc9371c1b70565cdfa93404219ae1890c16a62b
Ken Gullaksen <ken.gullaksen@nav.no>
2025-08-08 08:56:52 +0200

    explicitly tell jdk client to use system proxies

--- src/main/kotlin/no/nav/tag/innsynAareg/config/AppConfig.kt
+++ src/main/kotlin/no/nav/tag/innsynAareg/config/AppConfig.kt
@@ -7,7 +7,6 @@ import org.springframework.context.annotation.Configuration
 import org.springframework.web.client.RestTemplate
 import java.net.ProxySelector
 import java.net.http.HttpClient
-import java.util.function.Consumer
 
 
 @Configuration
@@ -25,6 +24,8 @@ class AppConfig {
      * This results in intermittent connection issues often resulting in errors like "org.springframework.web.client.ResourceAccessException: I/O error on POST request for https://login.microsoftonline.com..."
      *
      * To resolve this, we configure the `RestTemplate` to use a `ClientHttpRequestFactoryBuilder` that explicitly sets the proxy selector.
+     *
+     * This will only work if -Djava.net.useSystemProxies=true is set in the JVM options.
      */
     @Bean
     fun clientHttpRequestFactoryBuilder(): ClientHttpRequestFactoryBuilder<*> {
