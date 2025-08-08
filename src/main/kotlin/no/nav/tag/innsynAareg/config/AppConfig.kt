050f2b67b99a1b6fdf797cda34bf3c4aa6faa08f
Ken Gullaksen <ken.gullaksen@nav.no>
2025-08-08 09:34:55 +0200

    use simple client and document why

--- src/main/kotlin/no/nav/tag/innsynAareg/config/AppConfig.kt
+++ src/main/kotlin/no/nav/tag/innsynAareg/config/AppConfig.kt
@@ -1,12 +1,9 @@
 package no.nav.tag.innsynAareg.config
 
-import org.springframework.boot.http.client.ClientHttpRequestFactoryBuilder
 import org.springframework.boot.web.client.RestTemplateBuilder
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
 import org.springframework.web.client.RestTemplate
-import java.net.ProxySelector
-import java.net.http.HttpClient
 
 
 @Configuration
@@ -16,22 +13,4 @@ class AppConfig {
         return builder.build()
     }
 
-
-    /**
-     * As of Spring Boot 3.4 https://github.com/spring-projects/spring-boot/wiki/Spring-Boot-3.4-Release-Notes#upgrading-from-spring-boot-33
-     * The default `RestTemplate` uses the JDK HTTP client, which does not support proxy configuration via system properties in the same way the
-     * Simple JDK HttpURLConnection (SimpleClientHttpRequestFactory) did.
-     * This results in intermittent connection issues often resulting in errors like "org.springframework.web.client.ResourceAccessException: I/O error on POST request for https://login.microsoftonline.com..."
-     *
-     * To resolve this, we configure the `RestTemplate` to use a `ClientHttpRequestFactoryBuilder` that explicitly sets the proxy selector.
-     *
-     * This will only work if -Djava.net.useSystemProxies=true is set in the JVM options.
-     */
-    @Bean
-    fun clientHttpRequestFactoryBuilder(): ClientHttpRequestFactoryBuilder<*> {
-        return ClientHttpRequestFactoryBuilder.jdk()
-            .withHttpClientCustomizer { builder: HttpClient.Builder ->
-                builder.proxy(ProxySelector.getDefault())
-            }
-    }
 }
